1a5c7c09730981ab5218251fd75b34caa55c84fe
Tristan Nelson <tristan@iseult.us>
2016-11-23 10:40:36 -0500

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to garde-manger
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject garde-manger "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main ^:skip-aot garde-manger.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
