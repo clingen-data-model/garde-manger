578ce110fcfa7ed4bf52c1eeb565390c8e3cdae6
Tristan Nelson <tristan@iseult.us>
2017-04-21 21:22:51 -0400

    Adding example clinvar xml, begin parsing for CNVs

--- src/garde_manger/clinvar.clj
+++ src/garde_manger/clinvar.clj
@@ -4,22 +4,46 @@
             [clojure.java.io :as io]
             [clojure.zip :as zip]
             [clojure.data.zip :as dzip]
-            [clojure.data.zip.xml :as xdzip :refer [xml-> xml1-> attr attr= text]]))
+            [clojure.data.zip.xml :as xdzip :refer [xml-> xml1-> attr attr= text]]
+            [clojure.pprint :as pp :refer [pprint]]))
 
-(def cvxml "data/ClinVarFullRelease_2016-11.xml")
+;;(def cvxml "data/ClinVarFullRelease_2016-11.xml")
 ;;(def cvxml "data/RCV000077146.xml")
+(def cvxml "data/ClinVarFullRelease_2017-0417.xml")
 
 (defn import-clinvar
   "Read the ClinVar XML file, generate appropriate messages, and send to exchange"
   []
   (xml/parse (io/input-stream cvxml)))
 
-(defn rcv-title
+(defn rcv-type
   "return the title of the rcv"
   [el]
   (xml1-> (zip/xml-zip el)
-          :Title
-          text))
+          :ClinVarSet
+          :ReferenceClinVarAssertion
+          :MeasureSet
+          :Measure
+          (attr :Type)))
+
+(defn type=
+  "returns true if variant type equals the argument"
+  [node pred]
+  (xml1-> (zip/xml-zip node)
+          :ReferenceClinVarAssertion
+          :MeasureSet
+          :Measure
+          (attr= :Type pred)))
+
+(defn clinvar-cnvs
+  "Import variants from ClinVar, filter for CNVs"
+  []
+  (with-open [st (io/reader cvxml)]
+    (pprint (take 5  (->> st
+                       xml/parse
+                       :content
+                       (filter #(type= % "copy number gain"))
+                       (map rcv-type))))))
 
 ;; This code seems to avoid the 'head retention' problem
 ;; http://blog.korny.info/2014/03/08/xml-for-fun-and-profit.html#laziness---lose-your-head
